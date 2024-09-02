55f6dffa94ef40eb9ae3ae1ba3f769fdb3e1d4ae
Theodore R. Smith <theodore@phpexperts.pro>
2024-09-01 23:31:50 -0500

    Added the ability to filter 2FAs.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@
 import $ from 'jquery';
 
 $(function () {
+    $('#filter').on('input', function() {
+        const filterValue = $(this).val().toLowerCase();
+
+        $('.token').each(function() {
+            var tokenName = $(this).find('.token-name').text().toLowerCase();
+
+            if (tokenName.startsWith(filterValue)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
 });
 

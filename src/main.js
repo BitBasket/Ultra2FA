35fe6243e821117b8a7b97ddecb2a702bfbd088b
Theodore R. Smith <theodore@phpexperts.pro>
2024-09-02 07:00:58 -0500

    Load and display test 2FAs.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // Import jQuery from node_modules
 import $ from 'jquery';
 
+import * as otplib from 'otplib';
+
 $(function () {
+    load2FA();
+
+
     $('#filter').on('input', function() {
         const filterValue = $(this).val().toLowerCase();
 
@@ -17,3 +22,44 @@ $(function () {
     });
 });
 
+function generateOTPCode(secret)
+{
+    return otplib.authenticator.generate(secret);
+}
+
+function load2FA()
+{
+    download2FA().forEach((account) => {
+        const token = generateOTPCode(account.s);
+
+        const tokenDiv  = $('<div class="token"></div>');
+        const tokenName = $('<span class="token-name"></span>').text(account.a);
+        const tokenCode = $('<span class="token-code"></span>').text(token);
+        tokenDiv.append(tokenName).append(tokenCode);
+
+        $('.token-container').append(tokenDiv);
+    });
+}
+
+function download2FA()
+{
+    return [
+        { "a": "aa", "s":"AAAAAAAAAAAAAAAAA" },
+        { "a": "bb", "s":"IIIIIIIIIIIIIIIIIIIIIIIII" },
+        { "a": "eth(user)",  "s":"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB" },
+        { "a": "101domain", "s":"CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" },     
+        { "a": "GitHub.com (username)", "s":"DDDDDDDDDDDDDDDD" },
+        { "a": "ethcrash (Eg5G7yeEmC)", "s":"KJIWCVS3KBEFQXSVKQUHAMDGLY2HW3DDJQRTO5BQJRBUYNSGHRCA" },
+        { "a": "Bitwage.com (hopeseekr@gmail.com)", "s":"L2IJCDLI4QAHBWP4" },
+    ];
+}
+
+function decrypt2FA()
+{
+
+}
+
+function encrypt2FA()
+{
+
+}

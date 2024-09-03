0c98c093c43f0d862a456e7fddfea20c1547305b
Theodore R. Smith <theodore@phpexperts.pro>
2024-09-03 07:29:35 -0500

    Added loading / exporting encrypted 2FA data directly into localstorage and a downloaded file.

--- src/2fa.js
+++ src/2fa.js
@@ -2,14 +2,24 @@ import CryptoJS, { enc } from 'crypto-js';
 import $ from 'jquery';
 import * as otplib from 'otplib';
 
+// Show the popup when the Export button is clicked
+$('#export-btn').on('click', () => {
+    showPasskeyPopup(async passkey => {
+        const accounts = await download2FA();
+        const encrypted = encrypt2FA(JSON.stringify(accounts), passkey);
+        downloadExportFile('ultra-2fa.secrets.json.aes', encrypted.toString(CryptoJS.enc.Utf8))
+    });
+});
+
 function generateOTPCode(secret)
 {
     return otplib.authenticator.generate(secret);
 }
 
-function load2FA()
+async function load2FA()
 {
-    download2FA().forEach((account) => {
+    const accounts = await download2FA();
+    accounts.forEach((account) => {
         const token = generateOTPCode(account.s);
 
         const tokenDiv  = $('<div class="token"></div>');
@@ -23,32 +33,31 @@ function load2FA()
 
 function download2FA()
 {
-    let encrypted2FAData = localStorage.getItem('Ultra2FA.encrypted-data');
-    if (!encrypted2FAData) {
-        encrypted2FAData = 'U2FsdGVkX1+wKbXcRa4NkpedSum+gc7vmfujLDNCiqdgJ4V2itOF09xYyD1W7NUIoIBhoyuB3SKS6EWRujB2yl4FNk6SUQXRuresjPCw5fyZdt6kUh0InAAsSApvGuhz3z29nN5qEPLUSNG3p7QslAg7+Q/8S6mieYqnOnRA+bwfW5jYOhDVKX2pl9N+ntR8zn4JUNG4KOF8Xp8rtpSlMAicllSKf76iusB4M5i6LIh50DQnj53S9qva4/PA3fZhHRoey5tcAF0FjBhjQhb+e1TR3fPn3JXFVSX7mKpzVI74B9zMuk3EO1gwvQKGWfFF6U4bznC07xv9DuvSTrzEVzkZVFMEfvWk4aMH9iWzEYeXYViBZTRiYZ/tEFYPMucwrm3qTM0KCeXqNMius/wDhBjBiNQZMRuWALPefLhzhh0Qu2qTXpSioAf5iNIA3ocfbJBaea3JvVKh0qEFwK4hK9zVaL10dpitGz38VClguRFraUbk1J/LhTn5lRjregOAf9QRrlmVKYJUHZASyeqQ2ntzclmAUbhhdNfIzIU672XSZmxSJWzDtPD1Y53fx8MGQ8Wsh7aIO1r/5lIvDv9waeClQQqwqz9saCofPPvWj5E=';
-        // showFileUploadPopup(() => {
-        //     alert('File has been downloaded.');
-        // });
-    }
+    return new Promise(async (resolve, reject) => {
+        let encrypted2FAData = localStorage.getItem('Ultra2FA.encrypted-data');
+        if (encrypted2FAData === null) {
+            // Bootstrap: Uncomment and comment the showFileUploadPopup().
+            // encrypted2FAData = 'U2FsdGVkX1+wKbXcRa4NkpedSum+gc7vmfujLDNCiqdgJ4V2itOF09xYyD1W7NUIoIBhoyuB3SKS6EWRujB2yl4FNk6SUQXRuresjPCw5fyZdt6kUh0InAAsSApvGuhz3z29nN5qEPLUSNG3p7QslAg7+Q/8S6mieYqnOnRA+bwfW5jYOhDVKX2pl9N+ntR8zn4JUNG4KOF8Xp8rtpSlMAicllSKf76iusB4M5i6LIh50DQnj53S9qva4/PA3fZhHRoey5tcAF0FjBhjQhb+e1TR3fPn3JXFVSX7mKpzVI74B9zMuk3EO1gwvQKGWfFF6U4bznC07xv9DuvSTrzEVzkZVFMEfvWk4aMH9iWzEYeXYViBZTRiYZ/tEFYPMucwrm3qTM0KCeXqNMius/wDhBjBiNQZMRuWALPefLhzhh0Qu2qTXpSioAf5iNIA3ocfbJBaea3JvVKh0qEFwK4hK9zVaL10dpitGz38VClguRFraUbk1J/LhTn5lRjregOAf9QRrlmVKYJUHZASyeqQ2ntzclmAUbhhdNfIzIU672XSZmxSJWzDtPD1Y53fx8MGQ8Wsh7aIO1r/5lIvDv9waeClQQqwqz9saCofPPvWj5E=';
+            encrypted2FAData = await showFileUploadPopup()
+                .then((fileContent) => {
+                    // Store indefinitely in localStorage.
+                    localStorage.setItem('Ultra2FA.encrypted-data', fileContent)
+                    return fileContent;
+                });
+        }
 
-    showPasskeyPopup(passkey => {
-        import2FA(encrypted2FAData, passkey);
+        let accounts;
+        showPasskeyPopup(passkey => {
+            try {
+                data = import2FA(encrypted2FAData, passkey);
+                accounts = JSON.parse(data);
+            } catch (e) {
+                throw new Error("Invalid passkey / encrypted data:" + e);
+            }
+
+            resolve(accounts);
+        });
     });
-
-    return [
-        { "a": "aa", "s":"AAAAAAAAAAAAAAAAA" },
-        { "a": "bb", "s":"IIIIIIIIIIIIIIIIIIIIIIIII" },
-        { "a": "eth (user)",  "s":"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB" },
-        { "a": "101domain", "s":"CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" },     
-        { "a": "GitHub.com (username)", "s":"DDDDDDDDDDDDDDDD" },
-        { "a": "ethcrash (Eg5G7yeEmC)", "s":"KJIWCVS3KBEFQXSVKQUHAMDGLY2HW3DDJQRTO5BQJRBUYNSGHRCA" },
-        { "a": "Bitwage.com (hopeseekr@gmail.com)", "s":"L2IJCDLI4QAHBWP4" },
-    ];
-}
-
-function decrypt2FA()
-{
-
 }
 
 function encrypt2FA(text, secretKey)
@@ -111,14 +120,6 @@ function showPasskeyPopup(callback) {
         $('#passkey-popup').fadeOut();
         $('#passkey-popup-overlay').fadeOut();
     });
-
-    // Show the popup when the Export button is clicked
-    $('#export-btn').on('click', () => {
-        showPasskeyPopup(passkey => {
-            const encrypted = encrypt2FA(JSON.stringify(download2FA()), passkey);
-            downloadExportFile('ultra-2fa.secrets.json.aes', encrypted.toString(CryptoJS.enc.Utf8))
-        });
-    });
 }
 
 function downloadExportFile(filename, text) {
@@ -133,61 +134,63 @@ function downloadExportFile(filename, text) {
 
 function showFileUploadPopup(callback)
 {
-    // Show the popup and overlay
-    $('#file-upload-popup-overlay').fadeIn();
-    $('#file-upload-popup').fadeIn();
+    return new Promise((resolve, reject) => {
+        // Show the popup and overlay
+        $('#file-upload-popup-overlay').fadeIn();
+        $('#file-upload-popup').fadeIn();
 
-    // Handle form submission
-    $('#file-upload-form').off('submit').on('submit', function(event) {
-        event.preventDefault();
+        // Handle form submission
+        $('#file-upload-form').off('submit').on('submit', function(event) {
+            event.preventDefault();
 
-        // Get the selected file
-        const fileInput = $('#file-upload')[0];
-        const file = fileInput.files[0];
+            // Get the selected file
+            const fileInput = $('#file-upload')[0];
+            const file = fileInput.files[0];
 
-        // Check if a file was selected and if it has the correct .aes extension
-        if (file && file.name.endsWith('.aes')) {
-            // Execute the callback function with the file
+            // Check if a file was selected and if it has the correct .aes extension
+            if (file && file.name.endsWith('.aes')) {
+                // Execute the callback function with the file
 
-            // Read the file contents
-            const reader = new FileReader();
-            reader.onload = function(event) {
-                try {
-                    const fileContent = event.target.result;
-                    callback(fileContent); // Call the success callback with the file content
-                    $('#fileUploadPopup').remove(); // Remove popup after successful upload
-                } catch (error) {
-                    alert("Error reading the file.");
+                // Read the file contents
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    try {
+                        const fileContent = event.target.result;
+
+                        resolve(fileContent);
+
+                        // Remove popup after successful upload
+                        $('#fileUploadPopup').remove();
+                    } catch (error) {
+                        throw new Error("File reading failed.");
+                    }
+                };
+
+                reader.onerror = function() {
                     throw new Error("File reading failed.");
-                }
-            };
+                };
 
-            reader.onerror = function() {
-                alert("Error reading the file.");
-                throw new Error("File reading failed.");
-            };
+                reader.readAsText(file);
 
-            reader.readAsText(file);
+                // Clear the file input field (optional)
+                fileInput.value = '';
 
-            // Clear the file input field (optional)
-            fileInput.value = '';
+                // Hide the popup
+                $('#file-upload-popup').fadeOut();
+                $('#file-upload-popup-overlay').fadeOut();
+            } else {
+                alert('Please select a valid .aes file.');
+            }
+        });
 
-            // Hide the popup
+        // Close the popup when the overlay is clicked
+        $('#file-upload-popup-overlay').off('click').on('click', function() {
             $('#file-upload-popup').fadeOut();
             $('#file-upload-popup-overlay').fadeOut();
-        } else {
-            alert('Please select a valid .aes file.');
-        }
-    });
-
-    // Close the popup when the overlay is clicked
-    $('#file-upload-popup-overlay').off('click').on('click', function() {
-        $('#file-upload-popup').fadeOut();
-        $('#file-upload-popup-overlay').fadeOut();
+        });
     });
 }
 
 export { 
-    import2FA, download2FA, load2FA, 
-    decrypt2FA, encrypt2FA,
+    load2FA
 };
--- src/export.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-import * as TwoFA from './2fa.js';
-
-
-// Hide the popup when the overlay is clicked
